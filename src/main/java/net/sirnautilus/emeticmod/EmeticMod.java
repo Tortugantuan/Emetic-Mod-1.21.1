cfb426b909de0144e70b73f9c05994e0b94e9f43
Tortugantuan <aydblackwell@gmail.com>
2026-08-27 19:14:11 -0700

    Removed unnecessary template pieces

--- src/main/java/net/sirnautilus/emeticmod/EmeticMod.java
+++ src/main/java/net/sirnautilus/emeticmod/EmeticMod.java
@@ -1,26 +1,18 @@
 package net.sirnautilus.emeticmod;
 
 import net.sirnautilus.emeticmod.item.ModItems;
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(EmeticMod.MOD_ID)
 public class EmeticMod {
     public static final String MOD_ID = "emeticmod";
-    public static final Logger LOGGER = LogUtils.getLogger();
 
     public EmeticMod(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
